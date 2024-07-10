ca35721177aa9d7c1ed8c3f10dd4bc6d8658a484
selly361 <seikou615@gmail.com>
2024-07-10 18:15:12 +0100

    initial commit: set theme, install packages, remove default nca code

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ['@svgr/webpack']
+		})
+		return config
+	},
 
-export default nextConfig;
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'avatars.githubusercontent.com'
+			},
+			{
+				protocol: 'https',
+				hostname: 'lh3.googleusercontent.com'
+			}
+		]
+	}
+}
+
+export default nextConfig
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,107 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+	darkMode: 'class',
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}'
+	],
+	theme: {
+		extend: {
+			colors: {
+				bodyBg: 'var(--bodyBg)',
+				navBarBg: 'var(--navBarBg)',
+				asideBg: 'var(--asideBg)',
+				taskBg: 'var(--taskBg)',
+				scrollBg: 'var(--scrollBg)',
+
+				subTaskCheckBoxBg: 'var(--subTaskCheckBoxBg)',
+				subTaskCheckBoxHover: 'var(--subTaskCheckBoxHover)',
+				checkBoxBg: 'var(--checkBoxBg)',
+				completedCheckBoxBg: 'var(--completedCheckBoxBg)',
+				textFieldError: 'var(--textFieldError)',
+
+				dropDownBorder: 'var(--dropDownBorder)',
+				dropDownActive: 'var(--dropDownActive)',
+				dropDownBg: 'var(--dropDownBg)',
+
+				buttonPrimaryBg: 'var(--buttonPrimaryBg)',
+				buttonPrimaryHover: 'var(--buttonPrimaryHover)',
+				buttonPrimaryText: 'var(--buttonPrimaryText)',
+				buttonSecondaryBg: 'var(--buttonSecondaryBg)',
+				buttonSecondaryText: 'var(--buttonSecondaryText)',
+				buttonSecondaryHover: 'var(--buttonSecondaryHover)',
+				deleteButtonBg: 'var(--deleteButtonBg)',
+				deleteButtonText: 'var(--deleteButtonText)',
+				deleteButtonHover: 'var(--deleteButtonHover)',
+
+				textPrimary: 'var(--textPrimary)',
+
+				border: 'var(--border)',
+				grey: 'var(--grey)',
+				blue: 'var(--blue)',
+				lightBlue: 'var(--lightBlue)',
+				red: 'var(--red)',
+				normalTheme: 'var(--normalTheme)',
+				editDropDown: 'var(--editDropDown)'
+			},
+			fontSize: {
+				'heading-l': [
+					'36px',
+					{
+						lineHeight: '33px',
+						letterSpacing: '-1.125px',
+						fontWeight: '700'
+					}
+				],
+				'heading-m': [
+					'24px',
+					{
+						lineHeight: '22px',
+						letterSpacing: '-0.75px',
+						fontWeight: '700'
+					}
+				],
+				'heading-s': [
+					'15px',
+					{
+						lineHeight: '24px',
+						letterSpacing: '-0.25px',
+						fontWeight: '700'
+					}
+				],
+				'heading-s-variant': [
+					'15px',
+					{
+						lineHeight: '15px',
+						letterSpacing: '-0.25px',
+						fontWeight: '700'
+					}
+				],
+				body: [
+					'13px',
+					{
+						lineHeight: '18px',
+						letterSpacing: '-0.1px',
+						fontWeight: '500'
+					}
+				],
+				'body-variant': [
+					'13px',
+					{
+						lineHeight: '15px',
+						letterSpacing: '-0.1px',
+						fontWeight: '500'
+					}
+				]
+			},
+			backgroundImage: {
+				NewColumn: 'var(--NewColumn)'
+			}
+		}
+	},
+	plugins: []
+}
+
+export default config
