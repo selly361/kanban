3c53e24ddcbd71b6139afb8681938e91e6811fed
selly361 <seikou615@gmail.com>
2024-07-21 22:21:15 +0100

    Add `TaskManagementAnimation` component

---
+++ app/(auth)/login/TaskManagementAnimation.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import Lottie from 'lottie-react'
+import animation from '@/assets/animations/task-management.json'
+
+function TaskManagementAnimation() {
+	return <Lottie animationData={animation} loop={true} />
+}
+
+export default TaskManagementAnimation
